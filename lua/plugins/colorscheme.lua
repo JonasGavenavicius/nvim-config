231cfe8ad03e7e08ebfb9a9befd83dc01793a598
Jonas Gavėnavičius <jonasgav@Jonass-Macbook-M1-Pro-14.local>
2024-06-16 00:07:07 +0300

    backup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,4 @@
 return {
-  { "rose-pine/neovim" },
-
   {
     "rose-pine/neovim",
     lazy = true,
@@ -73,9 +71,19 @@ return {
     },
   },
   {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "github_dark_default",
     },
   },
 }
